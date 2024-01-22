9c5fa84dacc599dcd3a29267d5d1bd3e36a2c7f6
Moskas <minemoskas@gmail.com>
2024-01-22 01:11:58 +0100

    Added dynamic todo, added test asset.

--- vue-learn/src/App.vue
+++ vue-learn/src/App.vue
@@ -13,31 +13,59 @@ const editor = ref(true)
 function toggle() {
   editor.value = !editor.value
 }
+
+let id = 0
+const newTodo = ref('')
+const hideCompleted = ref(false)
+const todos = ref([{ id: id++, text: 'Learn Vue', done: false}])
+
+function addTodo() {
+  if (newTodo.value != "") {
+  todos.value.push({ id: id++, text: newTodo.value, done: false})
+  newTodo.value = ''
+  }
+}
+
+function removeTodo(todo) {
+  todos.value = todos.value.filter((t) => t !== todo)
+}
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <img alt="Vue logo" class="logo" src="./assets/nix.webp" width="125" height="125" />
   </header>
 
   <main>
-    <center>
       <h2 :class="titleClass">Testing</h2>
       <p>Count is: {{ counter.count }}</p>
-      <button @click="counter.count++">Click me</button>
-      <br />
+      <button @click="counter.count++">+</button>
+      <button @click="counter.count--">-</button>
       <input :value="text" @input="onInput" />
       <p :class="titleClass">{{ text }}</p>
       <button @click="toggle">Editor?</button>
-      <p v-if="editor">Emacs!</p>
+      <p v-if="editor">Emacs :)</p>
       <p v-else>Vim :(</p>
-    </center>
+      <p>
+        <h1>Todo-list</h1>
+        <form @submit.prevent="addTodo">
+          <input v-model="newTodo">
+          <button>Add Todo</button>
+        </form>
+        <ul>
+          <li v-for="todo in todos" :key="todo.id">
+            <input type="checkbox" v-model="todo.done">
+            <span :class="{ done: todo.done }">{{ todo.text }} </span>
+            <button @click="removeTodo(todo)">X</button>
+          </li>
+        </ul>
+      </p>
   </main>
 </template>
 
 <style scoped>
 * {
-  font-family: 'Iosevka Nerd Font';
+  font-family: 'JetBrainsMono Nerd Font';
 }
 
 header {
@@ -52,6 +80,10 @@ header {
 .test {
   color: #ebdbb2;
 }
+
+.done {
+  text-decoration: line-through;
+ }
 
 @media (min-width: 1024px) {
   header {
